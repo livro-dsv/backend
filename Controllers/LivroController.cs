96eeaa14a36e89b57b45f0797c08e514a2733215
Thaina Biudes <thaina.biudes@gmail.com>
2021-09-27 19:07:36 -0300

    feat: cria controller de livro

---
+++ Controllers/LivroController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+using API.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers 
+{
+  [ApiController]
+  [Route("api/livro")]
+
+  public class LivroController : ControllerBase
+  { 
+    private readonly DataContext _context;
+    public LivroController(DataContext context)
+    {
+      _context = context;
+    }
+
+    // POST: api/livro
+    [HttpPost]
+    [Route("create")]
+    public async Task<IActionResult> CreateAsync([FromBody] Livro livro)
+    {
+      await _context.Livros.AddAsync(livro);
+      await _context.SaveChangesAsync(); 
+    }
+
+    // GET: api/livro
+    [HttpGet]
+    [Route("list")]
+    public async Task<IActionResult> ListAsync()
+    {
+      return Ok(await _context.Livros.ToListAsync());
+    }
+  }
+}
