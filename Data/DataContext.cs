d05694e2615147ea7798c58d739c6a7189f15980
Thaina Biudes <thaina.biudes@gmail.com>
2021-09-27 19:07:56 -0300

    feat: cria data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+  public class DataContext : DbContext
+  {
+    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+    
+    public DbSet<Livro> Livros { get; set; }
+  }
+}
